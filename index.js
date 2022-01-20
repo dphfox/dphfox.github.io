917dfa30a71b8311b542b68e19034e0dc6e8c06f
Elttob <dan.bottlefact@gmail.com>
2022-01-20 00:57:37 +0000

    Remove js

--- index.js
+++
@@ -1,9 +0,0 @@
-let discordCopyLink = document.querySelector("#discord-copy-link");
-
-discordCopyLink.addEventListener("click", () => {
-	navigator.clipboard.writeText("Elttob#0001");
-	discordCopyLink.classList.add("copied");
-	setTimeout(() => {
-		discordCopyLink.classList.remove("copied");
-	}, 100);
-});
